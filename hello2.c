cb559ea97a7986692ad68e0595767af6684397a9
AbhirupPanda <evilseal09@gmail.com>
2022-06-09 19:25:11 +0530

    Update

--- hello2.c
+++ hello2.c
@@ -4,6 +4,7 @@ int main (int argc, char *argv[])
 {
   printf("Hello world!\n");
   printf("CE");
+  printf("B521006");
   return 0;
 }
 
--- helloworld.c
+++ helloworld.c
@@ -5,5 +5,6 @@ int main (int argc, char *argv[])
   printf("Hello world!\n");
   printf("Abhirup Panda");
   printf("B521006");
+  printf("CE")
   return 0;
 }
