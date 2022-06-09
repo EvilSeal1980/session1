c273b4942ffb2b234399a2fc1ccfe0e3ae30cfd7
AbhirupPanda <evilseal09@gmail.com>
2022-06-09 19:25:17 +0530

    Revert "Update"
    This reverts commit cb559ea97a7986692ad68e0595767af6684397a9.

--- hello2.c
+++ hello2.c
@@ -4,7 +4,6 @@ int main (int argc, char *argv[])
 {
   printf("Hello world!\n");
   printf("CE");
-  printf("B521006");
   return 0;
 }
 
--- helloworld.c
+++ helloworld.c
@@ -5,6 +5,5 @@ int main (int argc, char *argv[])
   printf("Hello world!\n");
   printf("Abhirup Panda");
   printf("B521006");
-  printf("CE")
   return 0;
 }
